1769bbc30f56c1034de5fa75ffcf6963e8181fd9
uma maheswararao <gumamaheswararao97@gmail.com>
2026-04-13 09:54:00 +0530

    Caluculator program

--- Script.js
+++ Script.js
@@ -73,3 +73,29 @@ const recipes= [{
             console.log(ar);
         }
     }
+ function add(num1,num2){
+        return num1;
+    }
+    console.log(add(10,20))//Hoisting
+   function add(num1,num2){
+        return num1+num2;
+    }
+    function m(){
+        return 3;
+    }
+    console.log(m)//returns function
+    console.log(m());
+    const add1=(a,b)=>{console.log( a+b)};
+    const sub=(a,b)=>{console.log( a-b)};
+    const mul=(a,b)=>{console.log( a*b)};
+    const div=(a,b)=>{console.log( a/b)};
+    const i=Number(prompt("Enter a"));
+    const j=Number(prompt("Enter b"));
+    const num18=prompt("enter num");
+    switch(num18){
+        case  "+":add1(i,j);break;
+        case  "-":sub(i,j);break;
+        case  "*":mul(i,j);break;
+        case  "/":div(i,j);break;
+        default:console.log("invalid");
+    }
