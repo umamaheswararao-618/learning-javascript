4e393040b889eca9591c18f3e28e65e0378ac46a
uma maheswararao <gumamaheswararao97@gmail.com>
2026-04-15 09:48:36 +0530

    js Events

--- Script.js
+++ Script.js
@@ -1,114 +1,140 @@
-console.log(-1);
-var num1=1234567891257578888;
-console.log(typeof num1);
-var num2=1234567891257578888n;
-console.log(typeof num2);
+// console.log(-1);
+// var num1=1234567891257578888;
+// console.log(typeof num1);
+// var num2=1234567891257578888n;
+// console.log(typeof num2);
 
-if(num1==num2) console.log(-1);
-console.log(2**3);
-function rprime(num)
-{
-    for(let i=2;i*i<=num;i++){
-        if(num%i==0)
-            return false;
-    }
-    return true;
-}
-let num=10;
-let p=2;
-for(let i=1;i<=num;){
-    if(rprime(p))
-    {
-        console.log(p+" ");
-        i++;
+// if(num1==num2) console.log(-1);
+// console.log(2**3);
+// function rprime(num)
+// {
+//     for(let i=2;i*i<=num;i++){
+//         if(num%i==0)
+//             return false;
+//     }
+//     return true;
+// }
+// let num=10;
+// let p=2;
+// for(let i=1;i<=num;){
+//     if(rprime(p))
+//     {
+//         console.log(p+" ");
+//         i++;
         
-    }
-    p++;
-}
-let a=[];
-for(let v=0;v<5;v++)
-{
-    if(v%2==1)
-    a.push(v);
-    else
-        a.unshift(v);
+//     }
+//     p++;
+// }
+// let a=[];
+// for(let v=0;v<5;v++)
+// {
+//     if(v%2==1)
+//     a.push(v);
+//     else
+//         a.unshift(v);
 
-}
-console.log(a);
-let b=[10,20,"uma"];
-console.log(b);
-b.pop();
-console.log(b);
-b.shift();
-console.log(b);
+// }
+// console.log(a);
+// let b=[10,20,"uma"];
+// console.log(b);
+// b.pop();
+// console.log(b);
+// b.shift();
+// console.log(b);
 
-const obj={
-    id:1,
-    name:"uma",
-    a:[1,2,3,"uma"]
-}
-console.log(obj.id);
-console.log(obj.a);
-const recipes= [{
-    id:1,
-    name:"uma",
-    a:[1,2,3,"uma"]
-    },
-{
-    id:3,
-    name:"uma",
-    a:[1,2,3,"uma"]
-},
-{
-    id:2,
-    name:"uma",
-    a:[1,2,3,"uma"]
-}]
-    for(const one in recipes){
-        console.log(one.id);
-    }
-   for(const one of recipes){
-        console.log(one.id);
-        for(const ar in one.a){
-            console.log(ar);
-        }
-    }
- function add(num1,num2){
-        return num1;
-    }
-    console.log(add(10,20))//Hoisting
-   function add(num1,num2){
-        return num1+num2;
-    }
-    function m(){
-        return 3;
-    }
-    // console.log(m)//returns function
-    // console.log(m());
-    // const add1=(a,b)=>{console.log( a+b)};
-    // const sub=(a,b)=>{console.log( a-b)};
-    // const mul=(a,b)=>{console.log( a*b)};
-    // const div=(a,b)=>{console.log( a/b)};
-    // const i=Number(prompt("Enter a"));
-    // const j=Number(prompt("Enter b"));
-    // const num18=prompt("enter num");
-    // switch(num18){
-    //     case  "+":add1(i,j);break;
-    //     case  "-":sub(i,j);break;
-    //     case  "*":mul(i,j);break;
-    //     case  "/":div(i,j);break;
-    //     default:console.log("invalid");
-    // }
-    // let name="uma"
-    // let age=18;
-    // console.log(`my name is ${name} and age ${age}`);//backtick
-   let dos= document.getElementById("hi");
-   let val=dos.textContent
-   //setTimeout(3000);
-   dos.innerText=val+"Lets See";
-   dos.innerHTML=val+"<sub>Let`s See</sub>"
+// const obj={
+//     id:1,
+//     name:"uma",
+//     a:[1,2,3,"uma"]
+// }
+// console.log(obj.id);
+// console.log(obj.a);
+// const recipes= [{
+//     id:1,
+//     name:"uma",
+//     a:[1,2,3,"uma"]
+//     },
+// {
+//     id:3,
+//     name:"uma",
+//     a:[1,2,3,"uma"]
+// },
+// {
+//     id:2,
+//     name:"uma",
+//     a:[1,2,3,"uma"]
+// }]
+//     for(const one in recipes){
+//         console.log(one.id);
+//     }
+//    for(const one of recipes){
+//         console.log(one.id);
+//         for(const ar in one.a){
+//             console.log(ar);
+//         }
+//     }
+//  function add(num1,num2){
+//         return num1;
+//     }
+//     console.log(add(10,20))//Hoisting
+//    function add(num1,num2){
+//         return num1+num2;
+//     }
+//     function m(){
+//         return 3;
+//     }
+//     // console.log(m)//returns function
+//     // console.log(m());
+//     // const add1=(a,b)=>{console.log( a+b)};
+//     // const sub=(a,b)=>{console.log( a-b)};
+//     // const mul=(a,b)=>{console.log( a*b)};
+//     // const div=(a,b)=>{console.log( a/b)};
+//     // const i=Number(prompt("Enter a"));
+//     // const j=Number(prompt("Enter b"));
+//     // const num18=prompt("enter num");
+//     // switch(num18){
+//     //     case  "+":add1(i,j);break;
+//     //     case  "-":sub(i,j);break;
+//     //     case  "*":mul(i,j);break;
+//     //     case  "/":div(i,j);break;
+//     //     default:console.log("invalid");
+//     // }
+//     // let name="uma"
+//     // let age=18;
+//     // console.log(`my name is ${name} and age ${age}`);//backtick
+//    let dos= document.getElementById("hi");
+//    let val=dos.textContent
+//    //setTimeout(3000);
+//    dos.innerText=val+"Lets See";
+//    dos.innerHTML=val+"<sub>Let`s See</sub>"
     
-   const boxes=document.getElementsByClassName("box");
-   for(let i=0;i<boxes.length;i++){
-    boxes[i].style.color = "red";
+//    const boxes=document.getElementsByClassName("box");
+//    for(let i=0;i<boxes.length;i++){
+//     boxes[i].style.color = "red";
+//    }
+ let val="uma maheswara rao";
+   const myFun123 = () =>{
+    let heading=document.getElementById("first1");
+   
+    let past=heading.textContent;
+    heading.innerText=val;
+    val=past;
+    
+   };
+   let col="red";
+   const myFun1 = () =>{
+    myFun123();
+    let heading=document.getElementById("first1");
+
+    let past=heading.style.color;
+    heading.style.color=col;
+    col=past;
+    
+   };
+   let   sr="https://i.pinimg.com/736x/25/3e/17/253e17972423290dcf626ae3a39e3194.jpg";
+   const imchange= () =>{
+    let img=document.getElementById("im");
+    let past=img.src;
+    img.src=sr;
+    sr=past;
    }
